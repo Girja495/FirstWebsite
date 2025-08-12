d5fde32f71734a6e054349ada33a2a3ac9ad870a
Girija495 <dhakad.girija15@gmail.com>
2025-08-13 00:28:02 +0530

    new site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,20 @@ def home():
 def products():
     return render_template('products.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+@app.route('/morephoto')
+def morephoto():
+    # List of images to display
+    images = ['jacket.png', 'jacket2.png', 'jacket3.png']
+    return render_template('morephoto.html', images=images)
 
+images = ['jacket.png', 'jacket2.png', 'jacket3.png']
+
+@app.route('/bigphoto')
+def bigphoto():
+    selected_img = request.args.get('img')
+    if selected_img not in images:
+        selected_img = images[0]  # default to first image if invalid or missing
+    return render_template('bigphoto.html', images=images, selected_img=selected_img)
+
+if __name__ == '__main__':
+    app.run(debug=True)
